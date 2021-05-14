1c9635a416a00c5d029b5cee7cf7c2268db73016
barleysack <hooker4020@naver.com>
2021-05-14 17:45:06 +0900

    Day 13 Done

--- Day13_Union.sql
+++ Day13_Union.sql
@@ -9,3 +9,10 @@ select cast(id as varchar), stdid from reginfotbl --형변환해야 가-능
 
 union
 select clubname, clubroom from clubtbl
+
+
+--EXCEPT - 2번째 쿼리에 해당하는 것을 제외함 
+select name, concat(mobile1,mobile2) as mb from userTBL
+except
+select name, concat(mobile1,mobile2) as mb from userTBL
+WHERE mobile1 is not null;
